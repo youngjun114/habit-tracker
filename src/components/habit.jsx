fb6504598637de574e5931b0de5c2d6e635f3cc1
youngjun114 <35579719+youngjun114@users.noreply.github.com>
2021-05-29 23:27:35 -0700

    added styling to buttons

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -6,6 +6,15 @@ export default class Habit extends Component {
       <>
         <span className='habit-name'>Reading</span>
         <span className='habit-count'>0</span>
+        <button className='habit-button habit-increase'>
+          <i className='fas fa-plus-square'></i>
+        </button>
+        <button className='habit-button habit-decrease'>
+          <i className='fas fa-minus-square'></i>
+        </button>
+        <button className='habit-button habit-delete'>
+          <i className='fas fa-trash'></i>
+        </button>
       </>
     );
   }
